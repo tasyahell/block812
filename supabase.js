c3920ce7e0cfb818953d41b93c11a0cb50ffe019
tasyahell <deckernst@icloud.com>
2026-04-19 18:26:23 +0300

    Add files via upload

---
+++ supabase.js
@@ -0,0 +1,3 @@
+const supabaseUrl = 'https://eizonbmilgxpofdqxwtd.supabase.co';
+const supabaseKey = 'sb_publishable_ZkbDCHdVGVyMWw3sQBLSMw_01vTc5cZ';
+const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
